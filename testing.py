c22d6fc94e58c2a0629ae3f885f1af3163570af1
abhinavkashyap92 <abhinavkashyap92@gmail.com>
2013-11-15 19:40:52 +0530

    Changed the structure

--- testing.py
+++ testing.py
@@ -1,19 +1,14 @@
-from OMPParallel import *
-from OMPFor import *
+from pyomp import *
 
 def main():
 	@OMPParallel(numprocs = 10, condition = True)
 	def parallel_block(*args, **kwargs):
 		a = [i for i in range(20, 80, 3)]
-
 		@OMPFor(args=args, kwargs=kwargs)
 		def for_block(a, *args, **kwargs):
 			for i in a:
 				print str(i) + " process ID: " + str(kwargs["procId"])
 
 		for_block(a)
-
-	parallel_block()
-
 if __name__ == '__main__':
 	main()
