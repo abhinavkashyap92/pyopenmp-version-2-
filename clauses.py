22cd84d8e2169d7ec6f57d0d163b4dea02a210dc
venkatesh <venkatesh@venkatesh.(none)>
2013-11-17 21:38:30 +0530

    Added shared clause to the pyomp module

--- clauses.py
+++ clauses.py
@@ -1,10 +1,59 @@
 import random
+import multiprocessing 
+
 """
 	*Private class for implementing the private clause
 	*
 	* 
 """
 
+class ClauseShared(object):
+	"""docstring for ClauseShared"""
+	def __init__(self, shared_variables,kwargs):
+		super(ClauseShared, self).__init__()
+		self.__shared_variables = shared_variables
+		self.__kwargs = kwargs
+
+	def get_shared_variables(self):
+		return self.__shared_variables
+
+	def get_kwargs(self):
+		return self.__kwargs
+
+	def createShared(self):
+		'''
+		Supports double,int,list and dictionary
+
+		'''
+		for eachVar in self.__shared_variables:
+			if type(eachVar[1]) is type([]):
+				self.__kwargs[eachVar[0]] = multiprocessing.Manager().list(eachVar[1])
+			elif type(eachVar[1]) is type({}):
+				self.__kwargs[eachVar[0]] = multiprocessing.Manager().dict(eachVar[1])
+		
+		return self.__kwargs
+
+	def copyBack(self):
+		for i in range(len(self.__shared_variables)):
+			if type(self.__shared_variables[i][1]) is type([]):
+				self.__shared_variables[i][1][:] = self.__kwargs[self.__shared_variables[i][0]]
+			elif type(self.__shared_variables[i][1]) is type({}):
+				self.__shared_variables[i][1].clear()
+				self.__shared_variables[i][1].update(self.__kwargs[self.__shared_variables[i][0]])
+
+#======================== How to use shared =====================================
+#           only list and dictionaries
+#	    list = [1,2,4]
+#	    dict = {'a':1}
+#	@OMPParallel(shared=(('list',list),('dict',dict))) # shared is a tuple of tuples each having name of the varible and its value - name can be anything
+#	def fun(*args,**kwargs):
+#		some code!
+# 		access by kwargs['name'] for list and dictionaries
+#		
+#	
+#================================================================================
+
+		
 class ClausePrivate(object):
 
 	"""
